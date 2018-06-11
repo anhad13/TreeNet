7e66ee92f229406d3bf25a995f0505c807eb2a4b
anhad13 <anhadmohananey@anhads-MacBook-Air.local>
2018-06-10 23:43:22 -0700

    snli2

--- dynet_snli.py
+++ dynet_snli.py
@@ -169,25 +169,29 @@ batch_size=64
 eval_every=batch_size*100
 training_data=read_dataset("/scratch/am8676/snli_1.0/snli_1.0_train.jsonl", vocab)
 dev_data=read_dataset("/scratch/am8676/snli_1.0/snli_1.0_dev.jsonl", vocab)
-trainer = dy.AdamTrainer(model)
+trainer = dy.AdamTrainer(model, 0.0003)
 treernn = TreeLSTMWO(model, len(vocab)+1, 300, 300)
 #import pdb;pdb.set_trace()
 treernn.E.init_from_array(np.array(vocab_embeddings))
 
-WfU=model.add_parameters((3, 300*2))
+WfU=model.add_parameters((300, 300*2))
+WfU2=model.add_parameters((3,300))
 import time
 dy.renew_cg()
 start_time=time.time()
-filename=open("dynetsnli-1.0", "w")
+filename=open("dynetsnli-1.0"+str(time), "w")
 losses=[]
 no_epochs=5
 for epoch_number in range(no_epochs):
     for i in range(len(training_data)):
         Wf=dy.parameter(WfU)
+        Wf2=dy.parameter(WfU2)
         d=training_data[i]
         t1, _=treernn.expr_for_tree(d[0], d[1])
         t2, _=treernn.expr_for_tree(d[2], d[3])
-        preds=Wf*dy.concatenate([t1,t2])
+        preds_1=Wf*dy.concatenate([t1,t2])
+        preds_1=dy.relu(preds_1)
+        preds=Wf2*preds_1
         losses.append(dy.pickneglogsoftmax(preds, d[4]))
         if i>0 and i%batch_size==0:
             batch_loss=dy.esum(losses)/len(losses)
@@ -210,7 +214,9 @@ for epoch_number in range(no_epochs):
                 d=dev_data[j]
                 t1, _=treernn.expr_for_tree(d[0], d[1])
                 t2, _=treernn.expr_for_tree(d[2], d[3])
-                preds=Wf*dy.concatenate([t1,t2])
+                preds_1=Wf*dy.concatenate([t1,t2])
+                preds_1=dy.relu(preds_1)
+                preds=Wf2*preds_1
                 results.append(preds)
                 actual_results.append(d[4])
                 if j>0 and j%batch_size==0:
